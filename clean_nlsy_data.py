b065feba82c1702562fcbd48872e57272366745a
augunes <ali.ulas.gunes@gmail.com>
2025-11-22 18:55:47 +0100

    Imported files from helloworldteam repository

---
+++ clean_nlsy_data.py
@@ -0,0 +1,14 @@
+def manage_nlsy_data(raw, info):
+    pass
+
+
+def _clean_one_wave(raw, year, info):
+    pass
+
+
+if __name__ == "__main__":
+    # load the raw dataset
+    # load the info
+    # call the cleaning function
+    # save the clean dataset in the bld folder
+    pass
